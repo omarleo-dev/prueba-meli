68a9b768c02a2e40f57b19c8bd69e35218532417
Jimmy Gonzalez <jimmygonzalez@C02FK9L7MD6M-Gonzalez.local>
2021-07-13 14:41:55 -0500

    Service con isMutant and validateDNA en service

--- src/test/java/com/meli/olbp/MutantControllerTest.java
+++ src/test/java/com/meli/olbp/MutantControllerTest.java
@@ -1,13 +1,17 @@
 package com.meli.olbp;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import reactor.core.publisher.Mono;
 
 @RunWith(SpringRunner.class)
 @WebFluxTest(controllers = MutantController.class)
@@ -17,8 +21,13 @@ public class MutantControllerTest {
 	@Autowired
     private WebTestClient webTestClient;
 	
+	@MockBean
+	MutantService mutantService;
+	
 	@Test
 	public void testMutantOK() {
+		when(mutantService.isMutant(new String[]{"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"}))
+		.thenReturn(Mono.just(true));
 		webTestClient.post()
 		.uri("/mutant")
 		.contentType(MediaType.APPLICATION_JSON)
@@ -46,6 +55,8 @@ public class MutantControllerTest {
 	
 	@Test
 	public void testMutantForbidden() {
+		when(mutantService.isMutant(new String[]{"ATGZGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"}))
+		.thenReturn(Mono.just(false));
 		webTestClient.post()
 		.uri("/mutant")
 		.contentType(MediaType.APPLICATION_JSON)
