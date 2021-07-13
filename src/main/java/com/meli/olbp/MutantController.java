c084ca6df193b2fbea753f6d44ae9b729c840f5b
Omar Barreta <omarleo.dev@gmail.com>
2021-07-13 15:16:44 -0500

    Service con isMutant and validateDNA en service

--- src/main/java/com/meli/olbp/MutantController.java
+++ src/main/java/com/meli/olbp/MutantController.java
@@ -4,47 +4,24 @@ import java.util.*;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequiredArgsConstructor
 public class MutantController {
+
+	@Autowired
+	private MutantService mutantService;
+
 	@PostMapping("/mutant")
 	public Mono<ResponseEntity> isMutant(@Valid @RequestBody MutantRequest request) {
-		return Mono.just(new ResponseEntity<>(
-				validateDNA(request.getDna())?
-					HttpStatus.OK:
-					HttpStatus.FORBIDDEN
-				));
+		return mutantService.isMutant(request.getDna()).map(e -> e?ResponseEntity.ok().build():ResponseEntity.status(HttpStatus.FORBIDDEN).build());
 	}
-	
 
-    /**
-     * This Method verify if the String[] have characters allowed and the length be correct
-     * @param dna
-     * @return true of false
-     */
-    public boolean validateDNA(String[] dna){
-        boolean validate = true;
-        int lengthChars = dna.length;
-        //
-        for (String line: dna) {
-            List<String> myCodes = new ArrayList<String>(Arrays.asList(line.split("")));
-            long discard = myCodes.stream()
-                    .filter(x -> x.matches("[^ATCGatcg]+"))
-                    .count();
-            if (line.length() != lengthChars || discard > 0){
-                validate = false;
-                break;
-            }                       
-        }
-        return  validate;
-    }
 }
